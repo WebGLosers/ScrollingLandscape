56d2e3dd0fcdfc7e5adf42cbe28b673ee675c9cb
Jason Nguyen <jatn@uw.edu>
2023-02-28 19:22:00 -0800

    Create foreground class

---
+++ src/my_game/objects/foreground.js
@@ -0,0 +1,37 @@
+"use strict"
+
+import engine from "../../engine"
+import Background from "./background";
+
+class Foreground extends Background {
+    constructor(spriteTexture, direction) {
+        this.kDelta = 0;
+        this.mRenderComponent = new engine.SpriteRenderable(spriteTexture);
+        this.mRenderComponent.setColor([1, 1, 1, 0]);
+
+        // Need to change
+        this.mRenderComponent.getXform().setPosition(50, 40);
+        this.mRenderComponent.getXform().setSize(3, 4);
+        
+        // Gets the direction for the background to move in
+        this.mDirection = direction;
+    }
+
+    setTint() {
+
+    }
+
+    setNewBackgroundImage(image) {
+
+    }
+
+    draw(camera) {
+
+    }
+
+    setSpeed() {
+
+    }
+}
+
+export default Foreground;
